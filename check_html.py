2279066a9e01771eb315a8eb34b59c7e8a68eb89
raj-patel-202 <rajking945272@gmail.com>
2026-08-31 23:06:57 +0530

    Optimize performance for Lighthouse and remove test files

--- check_html.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-r = urllib.request.urlopen('http://localhost:8000/files')
-html = r.read().decode('utf-8', 'ignore')
-print(f"<main in html: {'<main' in html}")
-print(f'id="main-content" in html: {"id=\"main-content\"" in html}')
-print(html[:500])
